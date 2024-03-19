fd1de7a8541ed7d16e3b92418e21ff6cc48a7b78
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-03-19 10:53:44 +0800

    attempt day 2

--- type_and_objects/practice_two.js
+++ type_and_objects/practice_two.js
@@ -1,27 +1,27 @@
 var Diet;
 (function (Diet) {
-    Diet["CARNIVORE"] = "Representing carnivorous animals";
-    Diet["HERBIVORE"] = "Representing herbivorous animals";
-    Diet["OMNIVORE"] = "Representing omnivorous animals";
+    Diet["CARNIVORE"] = "Representing CARNIVORE animals";
+    Diet["HEBIVORE"] = "Representing HEBIVORE animals";
+    Diet["OMNIVORE"] = "Representing OMNIVORE animals";
 })(Diet || (Diet = {}));
 var lion = {
     species: "Lion",
     name: "Simba",
     age: 5,
     displayDetails: function () {
-        console.log("ROAR LIONS DIET ".concat(Diet.CARNIVORE));
+        console.log("ROAR");
     },
 };
 var jungle = {
     type: "Jungle",
-    location: "Amazon Rainforest",
+    location: "Amazaon Rain Forest",
     temperature: 25,
     displayDetails: function () {
-        console.log("\n            HABITAT TYPE: ".concat(jungle.type, "\n            LOCATION: ").concat(jungle.location, "\n            TEMPERATURE: ").concat(jungle.temperature, "\n        "));
+        console.log("\n      TYPE: ".concat(jungle.type, "\n      LOCATION: ").concat(jungle.location, "\n      TEMPERATURE: ").concat(jungle.temperature, "\n      DIET:  ").concat(Diet.CARNIVORE, "\n    "));
     },
 };
-function interactWithHabitat(zooitem) {
-    zooitem.displayDetails();
+function interactWithHabitat(catcher, catcher1) {
+    catcher.displayDetails();
+    catcher1.displayDetails();
 }
-interactWithHabitat(lion);
-interactWithHabitat(jungle);
+interactWithHabitat(lion, jungle);
